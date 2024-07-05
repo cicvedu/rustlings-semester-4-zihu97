dd6d60505b6d13a4c35f09a414e87d0af6256aca
root <root@ubuntu5.15.myguest.virtualbox.org>
2024-07-05 22:05:59 +0800

    update

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,11 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = [1,2,3,4,5];
+    let a :[i32;1000] = [10;1000];
+    let a = ['a'; 10000];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
